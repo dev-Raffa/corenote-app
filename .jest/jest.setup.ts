4f276c7ebf10871a6dfedadca3ba35409f4bcda1
dev-Raffa <mnraffaell@gmail.com>
2024-04-16 12:23:38 -0300

    initial commit

---
+++ .jest/jest.setup.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+
+
+const a = 1;
+
+function carara(){
+    return "asnjsbifosndfklsdnosno aspfhipafoanf apfjpafpaf naofjpoan kafn afjpaofjapfn  afopapfoa npodjpoqjq fopafojf fpofojfpo"
+}
