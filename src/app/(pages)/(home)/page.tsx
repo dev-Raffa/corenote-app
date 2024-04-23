5d42dfb31ba2928632f5e59310e600d9eece5288
dev-Raffa <mnraffaell@gmail.com>
2024-04-23 08:05:05 -0300

    finish

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -5,41 +5,14 @@ import { useNotes } from '@/app/providers/post-its/context';
 import { BuildPostIts } from './components/postit/build';
 import { CreatePostIt } from './components/postit/create';
 import { Loader } from '@/app/components/load';
-import { useState } from 'react';
 
 export default function Home() {
-  const { notes, filtered, isLoading, setFiltered } = useNotes();
-  const colorOption = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const [showFilters, setShowFilters] = useState<boolean>(false);
-  const onChangeColorOption = (value: number) => {
-    if (value < 1 && filtered) {
-      setFiltered(undefined);
-      return;
-    }
-    setFiltered(notes.filter((note) => note.colorOption === value));
-  };
+  const { notes, filtered, isLoading } = useNotes();
 
   return (
     <main className="content">
       <section className="section-create">
         <CreatePostIt />
-      </section>
-      <section className="section-filters">
-        <button
-          className="filter-color"
-          onClick={() => setShowFilters(!showFilters)}
-        ></button>
-        <article className={`card-filter-color ${showFilters ? 'show' : ''}`}>
-          {colorOption.map((option) => {
-            return (
-              <button
-                className={`color-option-${option}`}
-                key={`select-color-option-${option}`}
-                onClick={() => onChangeColorOption(option)}
-              ></button>
-            );
-          })}
-        </article>
       </section>
       <section className="section-favorite">
         <h4>Favoritas</h4>
